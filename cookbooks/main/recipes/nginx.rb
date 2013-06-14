8ac81aceb9dedef300c1c6b15c450923dd3cdb79
Triet Le <trietminhle09@gmail.com>
2013-06-14 23:17:13 +0700

    install nginx

--- cookbooks/main/recipes/nginx.rb
+++ cookbooks/main/recipes/nginx.rb
@@ -1,1 +1,13 @@
 include_recipe "nginx::source"
+
+directory "/var/www" do
+  owner node['nginx']['user']
+end
+
+directory "/var/www/nginx-default" do
+end
+
+file "/var/www/nginx-default/index.html" do
+  owner node['user']['name']
+  content "<h1>Hello World</h1>"
+end
